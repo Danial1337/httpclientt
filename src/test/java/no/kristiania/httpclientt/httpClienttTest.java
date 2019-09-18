2e4590450ba6127f86d83af06c6771fbd1340317
Your Name <you@example.com>
2019-09-18 16:41:13 +0200

    ADD INITIAL TEST AND SUPPORT JUNIT5

---
+++ src/test/java/no/kristiania/httpclientt/httpClienttTest.java
@@ -0,0 +1,14 @@
+package no.kristiania.httpclientt;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class httpClienttTest {
+    @Test
+    void mathShouldWork(){
+        assertEquals(4, 2+2);
+
+    }
+}
